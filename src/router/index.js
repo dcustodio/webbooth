ccc4424af3e3708d5a4c94d4fa03f2b424209b99
David Custódio <davidlacustodio@gmail.com>
2018-10-06 12:25:20 +0200

    added gallery page

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/components/Home'
+import Gallery from '@/components/Gallery'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'Home',
       component: Home
+    },
+    {
+      path: '/gallery',
+      name: 'Galeria',
+      component: Gallery
     }
   ]
 })
